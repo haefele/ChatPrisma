6ef8cbcb5f1a174d349a1b325fb0a1017f5a93ed
Daniel Häfele <haefele@xemio.net>
2023-10-15 23:20:33 +0200

    Cleanup options and app startup code

---
+++ src/ChatPrisma/Options/UpdaterOptions.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatPrisma.Options;
+
+public class UpdaterOptions
+{
+    [Required]
+    public string GitHubUsername { get; set; } = default!;
+    [Required]
+    public string GitHubRepository { get; set; } = default!;
+    [Required]
+    public string GitHubReleaseAssetName { get; set; } = default!;
+}
