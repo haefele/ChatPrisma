eb75decb5cb4c4739db91845ea866f0f7a304c91
Daniel Häfele <haefele@xemio.net>
2023-10-23 20:30:39 +0200

    Improve TextEnhancementView.xaml startup positioning and auto-scrolling

--- src/ChatPrisma/Views/TextEnhancement/TextEnhancementView.xaml.cs
+++ src/ChatPrisma/Views/TextEnhancement/TextEnhancementView.xaml.cs
@@ -1,4 +1,7 @@
 using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using System.Windows.Threading;
 
 namespace ChatPrisma.Views.TextEnhancement;
 
@@ -7,6 +10,18 @@ public partial class TextEnhancementView
     private void TextEnhancementView_OnLoaded(object sender, RoutedEventArgs e)
     {
         this.InstructionTextBox.Focus();
+
+        // Place window slightly to the top
+        var window = Window.GetWindow(this)!;
+        window.Dispatcher.BeginInvoke(DispatcherPriority.Render, new Action(() =>
+        {
+            var helper = new WindowInteropHelper(window);
+            var currentScreen = Screen.FromHandle(helper.Handle);
+            var currentScreenHeight = currentScreen.Bounds.Height;
+
+            // Place the window a bit moved to the top, so it is perfectly centered if we reach this.MaxHeight
+            window.Top = Math.Max((currentScreenHeight - this.MaxHeight) / 2, 0);
+        }));
     }
 
     private void TextEnhancementView_OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
@@ -26,5 +41,11 @@ public partial class TextEnhancementView
     {
         // Set cursor at the end of the instruction textbox
         this.InstructionTextBox.Select(this.InstructionTextBox.Text.Length, 0);
+    }
+
+    private void TextTextBlock_OnSizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        // Keep the ScrollViewer scrolled to the bottom while the text is generating
+        this.TextScrollViewer.ScrollToVerticalOffset(this.TextScrollViewer.ExtentHeight);
     }
 }
