9e75a0f1975d4c3c593db9642a6de17cee76f8e7
Daniel Häfele <haefele@xemio.net>
2023-11-10 23:34:16 +0100

    Add "next-previous-version" feature to TextEnhancementView

--- src/ChatPrisma/Services/ChatBot/ChatPrompts.cs
+++ src/ChatPrisma/Services/ChatBot/ChatPrompts.cs
@@ -2,14 +2,13 @@ namespace ChatPrisma.Services.ChatBot;
 
 public static class ChatPrompts
 {
-    public static string System(string text) => $"""
-                                                You are "Chat Prisma," an expert assistant focused on helping users improve their written text. 
-                                                Your task is to fine-tune the user's text according to their precise specifications. 
-                                                Respond exclusively with the improved text, omitting any additional commentary.
-                                                Collaborate with the user in iterative cycles until the user is satisfied.
-                                                Always build upon the most recent version of the text, rather than reverting to the original.
-                                                
-                                                The current iteration of the text is as follows:
-                                                {text}
-                                                """;
+    public static string System() => """
+                                     You are "Chat Prisma," an expert assistant focused on helping users improve their written text. 
+                                     Your task is to fine-tune the user's text according to their precise specifications. 
+                                     Respond exclusively with the improved text, omitting any additional commentary.
+                                     Collaborate with the user in iterative cycles until the user is satisfied.
+                                     Always build upon the most recent version of the text, rather than reverting to the original.
+                                     
+                                     The current iteration of the text is in the next message.
+                                     """;
 }
---
+++ src/ChatPrisma/Themes/Stepper.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ChatPrisma.Themes;
+
+public partial class Stepper
+{
+    public Stepper()
+    {
+        this.InitializeComponent();
+    }
+
+    public static readonly DependencyProperty PreviousCommandProperty = DependencyProperty.Register(
+        nameof(PreviousCommand), typeof(ICommand), typeof(Stepper), new PropertyMetadata(default(ICommand)));
+
+    public ICommand PreviousCommand
+    {
+        get { return (ICommand)GetValue(PreviousCommandProperty); }
+        set { SetValue(PreviousCommandProperty, value); }
+    }
+
+    public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+        nameof(Value), typeof(int), typeof(Stepper), new PropertyMetadata(default(int)));
+
+#pragma warning disable CA1721
+    public int Value
+    {
+        get { return (int)GetValue(ValueProperty); }
+        set { SetValue(ValueProperty, value); }
+    }
+#pragma warning restore CA1721
+
+    public static readonly DependencyProperty NextCommandProperty = DependencyProperty.Register(
+        nameof(NextCommand), typeof(ICommand), typeof(Stepper), new PropertyMetadata(default(ICommand)));
+
+    public ICommand NextCommand
+    {
+        get { return (ICommand)GetValue(NextCommandProperty); }
+        set { SetValue(NextCommandProperty, value); }
+    }
+}
