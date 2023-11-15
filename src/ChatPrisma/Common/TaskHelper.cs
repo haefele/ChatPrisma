f9460f6fa53c0de47a0c75d3d853cadd5f3356cc
Daniel Häfele <haefele@xemio.net>
2023-11-15 13:36:49 +0100

    Cleanup "WaitUntil..." methods in ClipboardTextExtractor.cs and also add a WaitUntil to the SendKeysClipboardTextWriter.cs

---
+++ src/ChatPrisma/Common/TaskHelper.cs
@@ -0,0 +1,33 @@
+﻿namespace ChatPrisma.Common;
+
+public static class TaskHelper
+{
+    public static async Task<bool> WaitUntil(Func<bool> condition, int timeoutInMilliseconds)
+    {
+        var result = await WaitUntil(() => (condition(), string.Empty), timeoutInMilliseconds);
+        return result.Success;
+    }
+    public static async Task<(bool Success, T? Data)> WaitUntil<T>(Func<(bool, T)> condition, int timeoutInMilliseconds)
+    {
+        using var cancellationTokenSource = new CancellationTokenSource(timeoutInMilliseconds);
+        var cancellationToken = cancellationTokenSource.Token;
+
+        try
+        {
+            // Wait until we reach the timeout or condition is true
+            while (true)
+            {
+                var (success, data) = condition();
+                if (success)
+                    return (true, data);
+
+                // This delay is cancellable and will throw an exception if the token is cancelled.
+                await Task.Delay(10, cancellationToken);
+            }
+        }
+        catch (TaskCanceledException)
+        {
+            return (Success: false, Data: default);
+        }
+    }
+}
