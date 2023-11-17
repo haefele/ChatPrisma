40da1ab79c08e7cb4ca9afc7218fafb9a1bfada8
Daniel Häfele <haefele@xemio.net>
2023-11-17 15:42:19 +0100

    Make UpdateOptionsService more resilient to "non perfect" json structures in config file (trailing commas and comments)

--- src/ChatPrisma/Services/UpdateOptions/UpdateOptionsService.cs
+++ src/ChatPrisma/Services/UpdateOptions/UpdateOptionsService.cs
@@ -3,10 +3,11 @@ using System.Text.Json;
 using System.Text.Json.Nodes;
 using ChatPrisma.Options;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace ChatPrisma.Services.UpdateOptions;
 
-public class UpdateOptionsService(IHostEnvironment hostEnvironment) : IUpdateOptionsService
+public class UpdateOptionsService(IHostEnvironment hostEnvironment, ILogger<UpdateOptionsService> logger) : IUpdateOptionsService
 {
     public async Task Update(OpenAIOptions options)
     {
@@ -20,25 +21,37 @@ public class UpdateOptionsService(IHostEnvironment hostEnvironment) : IUpdateOpt
     private async Task UpdateSettings(Action<JsonNode> updateAction)
     {
         var path = Path.Combine(AppContext.BaseDirectory, $"appsettings.{hostEnvironment.EnvironmentName}.json");
+        logger.LogInformation("Updating config at {Path}", path);
 
         string settingsContent;
         try
         {
             settingsContent = await File.ReadAllTextAsync(path);
         }
-        catch (FileNotFoundException)
+        catch (FileNotFoundException exception)
         {
+            logger.LogError(exception, "Could not read existing config file from {Path}, starting with a new empty one", path);
             settingsContent = "{}";
         }
 
-        var json = JsonNode.Parse(settingsContent) ?? new JsonObject();
-
-        updateAction(json);
-
-        await using var writer = File.CreateText(path);
-        await writer.WriteAsync(json.ToJsonString(new JsonSerializerOptions
+        try
         {
-            WriteIndented = true
-        }));
+            var json = JsonNode.Parse(settingsContent, documentOptions: new JsonDocumentOptions { AllowTrailingCommas = true, CommentHandling = JsonCommentHandling.Skip }) ?? new JsonObject();
+
+            updateAction(json);
+
+            await using var writer = File.CreateText(path);
+            await writer.WriteAsync(json.ToJsonString(new JsonSerializerOptions
+            {
+                WriteIndented = true
+            }));
+
+            logger.LogInformation("Updated config at {Path} successfully!", path);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "An unexpected error occurred while updating the config at {Path}", path);
+            throw new PrismaException($"Ein unerwarteter Fehler ist beim Speichern der Einstellungen aufgetreten.", e);
+        }
     }
 }
