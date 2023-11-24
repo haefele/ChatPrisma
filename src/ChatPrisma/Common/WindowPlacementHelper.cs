6e0a5f02e59123db817fbbf638073e1bd5b7a3d8
Daniel Häfele <haefele@xemio.net>
2023-11-24 16:30:27 +0100

    Move window placement logic into a shared helper class

---
+++ src/ChatPrisma/Common/WindowPlacementHelper.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace ChatPrisma.Common;
+
+public static class WindowPlacementHelper
+{
+    public static void CenteredInFront(FrameworkElement view)
+    {
+        var window = Window.GetWindow(view)!;
+
+        // Setup dimensions and starting position
+        window.Dispatcher.BeginInvoke(DispatcherPriority.Render, () =>
+        {
+            var helper = new WindowInteropHelper(window);
+            var currentScreen = Screen.FromHandle(helper.Handle);
+            var dpi = VisualTreeHelper.GetDpi(window);
+            var currentScreenWorkingAreaDpiAdjusted = new Rectangle(
+                (int)(currentScreen.WorkingArea.X / dpi.DpiScaleX),
+                (int)(currentScreen.WorkingArea.Y / dpi.DpiScaleY),
+                (int)(currentScreen.WorkingArea.Width / dpi.DpiScaleX),
+                (int)(currentScreen.WorkingArea.Height / dpi.DpiScaleY));
+
+            view.Width = currentScreenWorkingAreaDpiAdjusted.Width switch
+            {
+                > 1400 => 800,
+                _ => 600
+            };
+
+            view.MinHeight = 200;
+
+            // Set window max-height, so we don't have to think about the window-shell when calculating the starting position
+            window.MaxHeight = currentScreenWorkingAreaDpiAdjusted.Height switch
+            {
+                > 1200 => 1000,
+                > 1000 => 800,
+                > 700 => 600,
+                _ => 400,
+            };
+
+            // Place the window a bit moved to the top, so it is perfectly centered if we reach window.MaxHeight
+            window.Top = currentScreenWorkingAreaDpiAdjusted.Y + Math.Max((currentScreenWorkingAreaDpiAdjusted.Height - window.MaxHeight) / 2, 0);
+            // And horizontally perfectly centered, because we have a fixed width
+            window.Left = currentScreenWorkingAreaDpiAdjusted.X + Math.Max((currentScreenWorkingAreaDpiAdjusted.Width - view.Width) / 2, 0);
+        });
+
+        // Hide the window until the previous call has positioned it correctly
+        // Don't use Visibility here, as that will not just hide the window, but also deactivate it and make it "not be a dialog anymore"
+        window.Opacity = 0;
+        window.Dispatcher.BeginInvoke(DispatcherPriority.Render, () =>
+        {
+            window.Opacity = 1;
+        });
+
+        // Ensure window is shown above all other windows
+        window.Activate();
+        window.Topmost = true;
+        window.Topmost = false;
+        window.Focus();
+    }
+}
--- src/ChatPrisma/Views/TextEnhancement/TextEnhancementView.xaml.cs
+++ src/ChatPrisma/Views/TextEnhancement/TextEnhancementView.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System.Windows;
-using System.Windows.Forms;
-using System.Windows.Interop;
-using System.Windows.Media;
 using System.Windows.Threading;
+using ChatPrisma.Common;
 
 namespace ChatPrisma.Views.TextEnhancement;
 
@@ -12,59 +10,10 @@ public partial class TextEnhancementView
     {
         this.InstructionTextBox.Focus();
 
-        var window = Window.GetWindow(this)!;
-
         // Ensure we are scrolled to the bottom
-        window.Dispatcher.BeginInvoke(DispatcherPriority.Render, this.ScrollToBottom);
+        this.Dispatcher.BeginInvoke(DispatcherPriority.Render, this.ScrollToBottom);
 
-        // Setup dimensions and starting position
-        window.Dispatcher.BeginInvoke(DispatcherPriority.Render, () =>
-        {
-            var helper = new WindowInteropHelper(window);
-            var currentScreen = Screen.FromHandle(helper.Handle);
-            var dpi = VisualTreeHelper.GetDpi(window);
-            var currentScreenWorkingAreaDpiAdjusted = new Rectangle(
-                (int)(currentScreen.WorkingArea.X / dpi.DpiScaleX),
-                (int)(currentScreen.WorkingArea.Y / dpi.DpiScaleY),
-                (int)(currentScreen.WorkingArea.Width / dpi.DpiScaleX),
-                (int)(currentScreen.WorkingArea.Height / dpi.DpiScaleY));
-
-            this.Width = currentScreenWorkingAreaDpiAdjusted.Width switch
-            {
-                > 1400 => 800,
-                _ => 600
-            };
-
-            this.MinHeight = 200;
-
-            // Set window max-height, so we don't have to think about the window-shell when calculating the starting position
-            window.MaxHeight = currentScreenWorkingAreaDpiAdjusted.Height switch
-            {
-                > 1200 => 1000,
-                > 1000 => 800,
-                > 700 => 600,
-                _ => 400,
-            };
-
-            // Place the window a bit moved to the top, so it is perfectly centered if we reach window.MaxHeight
-            window.Top = currentScreenWorkingAreaDpiAdjusted.Y + Math.Max((currentScreenWorkingAreaDpiAdjusted.Height - window.MaxHeight) / 2, 0);
-            // And horizontally perfectly centered, because we have a fixed width
-            window.Left = currentScreenWorkingAreaDpiAdjusted.X + Math.Max((currentScreenWorkingAreaDpiAdjusted.Width - this.Width) / 2, 0);
-        });
-
-        // Hide the window until the previous call has positioned it correctly
-        // Don't use Visibility here, as that will not just hide the window, but also deactivate it and make it "not be a dialog anymore"
-        window.Opacity = 0;
-        window.Dispatcher.BeginInvoke(DispatcherPriority.Render, () =>
-        {
-            window.Opacity = 1;
-        });
-
-        // Ensure window is shown above all other windows
-        window.Activate();
-        window.Topmost = true;
-        window.Topmost = false;
-        window.Focus();
+        WindowPlacementHelper.CenteredInFront(this);
     }
 
     private void TextEnhancementView_OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
